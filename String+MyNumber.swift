648cb423ed455e8bd4f802148ebdcb1110cfebb5
Kazumasa Kojima <kojima@bugnitude.com>
2019-07-15 20:53:27 +0900

    Update String+MyNumber.swift

--- String+MyNumber.swift
+++ String+MyNumber.swift
@@ -1,8 +1,8 @@
 extension String {
-
+	
 	var isMyNumber: Bool {
 		var numbers = [Int]()
-		for character in self.characters {
+		for character in self {
 			if let number = Int(String(character), radix: 10) {
 				numbers.append(number)
 			}
@@ -10,11 +10,11 @@ extension String {
 				return false
 			}
 		}
-
+		
 		if numbers.count != 12 {
 			return false
 		}
-
+		
 		let checkDigit = numbers.popLast()!
 		var sum = 0
 		for (i, p) in numbers.reversed().enumerated() {
@@ -22,9 +22,10 @@ extension String {
 			let q = n <= 6 ? n + 1 : n - 5
 			sum += p * q
 		}
+		
 		let remainder = sum % 11
 		let calculatedCheckDigit = remainder <= 1 ? 0 : 11 - remainder
 		return checkDigit == calculatedCheckDigit
 	}
-
+	
 }
